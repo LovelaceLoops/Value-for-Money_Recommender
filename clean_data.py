4a9281b8ddd9bc18520ba2017ad42820ba24ede4
KartikPatil22 <kartikpatil220706@gmail.com>
2026-09-15 11:15:14 +0530

    Outliers Detection and Price Data

--- clean_data.py
+++ clean_data.py
@@ -8,3 +8,11 @@ print("\n" + "========== LOAD REAL ESTATE DATASET ==========")
 data = pd.read_csv("Real estate.csv")
 print(data.head())
 print(data.columns)
+
+#Check for outliers and understand spread of price data
+print("\n" + "========== OUTLIERS DETECTION AND PRICE DATA ==========")
+plt.figure(figsize=(6, 4))
+sns.boxplot(y=data['Y house price of unit area'], color='#4C72B0')
+plt.title("Price Distribution — Outlier Check")
+plt.tight_layout()
+plt.show()
